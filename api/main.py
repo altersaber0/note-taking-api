a79f86401b2172563a5ee80f1641eb498f975c08
altersaber26 <altersaber26@gmail.com>
2023-01-12 15:53:46 +0200

    Setup api routers

--- api/main.py
+++ api/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 
 from .database.db_setup import engine, Base
+from .routers import notes, groups, users, auth
 
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.include_router(notes.router)
+app.include_router(groups.router)
+app.include_router(users.router)
+app.include_router(auth.router)
