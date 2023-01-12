7fcb618a45608a071e996463b742d4af800b70c0
altersaber26 <altersaber26@gmail.com>
2023-01-12 19:24:27 +0200

    Created API endpoint for user registration (signup)

--- api/database/models.py
+++ api/database/models.py
@@ -11,7 +11,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, nullable=False) # auto-increment automatically
     email = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
-    created_at = Column(DateTime, nullable=False, default=datetime.now)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
 
 
 class Group(Base):
@@ -20,7 +20,7 @@ class Group(Base):
     id = Column(Integer, primary_key=True, nullable=False) # auto-increment automatically
     name = Column(String, nullable=False)
     user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
-    created_at = Column(DateTime, nullable=False, default=datetime.now)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
 
 
 class Note(Base):
@@ -31,4 +31,4 @@ class Note(Base):
     content = Column(String, nullable=False)
     group_id = Column(Integer, ForeignKey("groups.id", ondelete="CASCADE"), nullable=False)
     user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
-    created_at = Column(DateTime, nullable=False, default=datetime.now)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
--- api/routers/users.py
+++ api/routers/users.py
@@ -1,13 +1,31 @@
 from fastapi import APIRouter, HTTPException, status, Depends
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
 
 from ..schemas import user as user_schema
 from ..database.db_setup import get_db
+from ..database.models import User
+from ..database import password
 
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
 
 @router.post("/", response_model=user_schema.UserInfo)
-def sign_up(user: user_schema.UserCreate, db: Session = Depends(get_db)):
-    ...
+def sign_up(new_user: user_schema.UserCreate, db: Session = Depends(get_db)):
+
+    try:
+        new_user.password = password.hash(new_user.password)
+        user = User(**new_user.dict())
+        db.add(user)
+        db.commit()
+        
+    # except when UNIQUE constraint on users.email fails
+    except IntegrityError:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="User with this email already exists"
+        )
+    
+    db.refresh(user)
+    return user
