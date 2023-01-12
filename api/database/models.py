336c47a14239a15b49f3a10bce087e77c5899a59
altersaber26 <altersaber26@gmail.com>
2023-01-12 16:17:59 +0200

    Create sqlalchemy models (db tables) of users, groups and notes

---
+++ api/database/models.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+
+from sqlalchemy import Column, ForeignKey, String, Integer, DateTime
+
+from .db_setup import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, nullable=False) # auto-increment automatically
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.now)
+
+
+class Group(Base):
+    __tablename__ = "groups"
+
+    id = Column(Integer, primary_key=True, nullable=False) # auto-increment automatically
+    name = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.now)
+
+
+class Note(Base):
+    __tablename__ = "notes"
+
+    id = Column(Integer, primary_key=True, nullable=False) # auto-increment automatically
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    group_id = Column(Integer, ForeignKey("groups.id", ondelete="CASCADE"), nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.now)
