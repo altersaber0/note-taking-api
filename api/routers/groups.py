6ca6e9831f8fbbc6f00182c60e31901a6aec5c13
altersaber26 <altersaber26@gmail.com>
2023-01-13 20:53:35 +0200

    Implement PUT on /groups/{id} endpoint for updating groups by id

--- api/routers/groups.py
+++ api/routers/groups.py
@@ -32,3 +32,32 @@ def create_group(
     return new_group
 
 
+@router.put("/{id}", response_model=group_schema.GroupInfo)
+def update_group_by_id(
+    id: int,
+    updated_group: group_schema.GroupCreate,
+    db: Session = Depends(get_db),
+    current_user_id: int = Depends(get_current_user_id)
+):
+
+    query = db.query(Group).filter(Group.id == id)
+
+    group = query.first()
+
+    if not group:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Requested group id does not exist"
+        )
+    
+    if group.user_id != current_user_id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Not allowed to update other users' groups"
+        )
+
+    query.update(updated_group.dict())
+    db.commit()
+    db.refresh(group)
+    
+    return group
