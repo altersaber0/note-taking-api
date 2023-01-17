ea68b8c60b13db5e284e9d8e625336a78169ffcd
altersaber26 <altersaber26@gmail.com>
2023-01-17 23:51:26 +0200

    Add CORS middleware

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .database.db_setup import engine, Base
 from .routers import notes, groups, users, auth
@@ -13,6 +14,13 @@ app = FastAPI(
     A small CRUD API for creating and managing text notes & groups of notes.
     You can sign up on POST /users to later get authenticated on POST /login.
     """
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 app.include_router(notes.router)
