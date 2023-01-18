80f1f19d80ee3af7bb2568270140fe15bcc9ecf9
altersaber26 <altersaber26@gmail.com>
2023-01-18 20:57:57 +0200

    Write tests for PUT and DELETE on /users/{id}

--- tests/test_users.py
+++ tests/test_users.py
@@ -41,3 +41,29 @@ def test_get_user_by_id(authenticated_client, id, status_code):
     response = authenticated_client.get(f"/users/{id}")
 
     assert response.status_code == status_code
+
+
+@pytest.mark.parametrize("updated_data, id, status_code", [
+    ({}, 1, 422),
+    ({"fhsd": "fsdf", "sfsfs": "sdfs"}, 1, 422),
+    ({"email": "example2@gmail.com", "password": "qwerty123"}, 1, 200),
+    ({"email": "example2@gmail.com", "password": "qwerty123"}, 2, 404),
+    ({"email": "example2@gmail.com", "password": "qwerty123"}, "qwe", 422),
+    ({"email": "example2@gmail.com", "password": "qwerty123"}, "", 405)
+])
+def test_update_user(authenticated_client, updated_data, id, status_code):
+    response = authenticated_client.put(f"/users/{id}", json=updated_data)
+
+    assert response.status_code == status_code
+
+
+@pytest.mark.parametrize("id, status_code", [
+    (1, 204),
+    (2, 404),
+    ("qwe", 422),
+    ("", 405)
+])
+def test_delete_user(authenticated_client, id, status_code):
+    response = authenticated_client.delete(f"/users/{id}")
+    
+    assert response.status_code == status_code
